37b188b9008954a72d830e03429346f5538b5490
Julia Simakina <simakina.js@yandex.ru>
2023-09-09 23:36:40 +0300

    fix: ревью 1 итерация

--- backend/app.js
+++ backend/app.js
@@ -18,13 +18,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
-})
-  .then(() => {
-    console.log('БД подключена');
-  })
-  .catch(() => {
-    console.log('БД не подключилась');
-  });
+});
 
 const app = express();
 
